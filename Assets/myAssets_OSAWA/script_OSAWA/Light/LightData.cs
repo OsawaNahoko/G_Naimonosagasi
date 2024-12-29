079e7b0e0259e56f798c530918fd27c4d95f5973
SioazinoNapoporitan <nahoko58osawa@gmail.com>
2024-12-29 17:53:30 +0900

    コミット欄をきれいにするため削除した経歴だけプッシュします。

--- Assets/myAssets_OSAWA/script_OSAWA/Light/LightData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "LightData", menuName = "LightData")]
-public class LightData : ScriptableObject
-{
-    public bool isLighting;
-}
