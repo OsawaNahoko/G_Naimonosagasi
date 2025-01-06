21236bd51e89f63b56c8ca30444ca68c0fce3015
SioazinoNapoporitan <nahoko58osawa@gmail.com>
2025-01-06 17:45:23 +0900

    エラーが発生したため小分けにしてコミットしています。 ・Lineが表示されないバグを完全解決しました。α値を０にしてしまってました。 ・ファイル整理をきれいにしました。

--- UnityChanSSU_URP-release-1.0.5/Assets/myAssets_OSAWA/script/GameManager/GetObjectData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AllObjectData", menuName = "AllObjectData")]
-public class AllObjectData : ScriptableObject
-{
-    public int AllObjectCount;
-}
