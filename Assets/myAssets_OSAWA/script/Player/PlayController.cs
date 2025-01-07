59b2013e7e9a6eaaa2744e44401435c24660251f
SioazinoNapoporitan <nahoko58osawa@gmail.com>
2025-01-07 09:22:45 +0900

    Xboxコントローラーを適用させました！！

--- Assets/myAssets_OSAWA/script/Player/PlayController.cs
+++ Assets/myAssets_OSAWA/script/Player/PlayController.cs
@@ -9,6 +9,7 @@ public class PlayController : MonoBehaviour
 
     [SerializeField] float _Speed;           //Playreの移動速度
     [SerializeField] float _mouseSensitivity;//Mousecasollの移動速度
+    [SerializeField] float _ContrerSenstivity;//コントローラーの移動速度
 
     
     [SerializeField] Transform camTrans;
@@ -37,7 +38,26 @@ public class PlayController : MonoBehaviour
         
         transform.rotation = Quaternion.Euler(this.transform.rotation.eulerAngles.x, this.transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
         camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+               
+        //カメラの回転制御　コントローラー;
+        Vector2 ContlerInput = new Vector2(Input.GetAxisRaw("Horizontal2"), Input.GetAxisRaw("Vertical2")) * _ContrerSenstivity;
+                
+        transform.rotation = Quaternion.Euler(this.transform.rotation.eulerAngles.x, this.transform.rotation.eulerAngles.y + ContlerInput.x, transform.rotation.eulerAngles.z);
+        camTrans.rotation = Quaternion.Euler(camTrans.rotation.eulerAngles + new Vector3(ContlerInput.y, 0f, 0f));
 
+        if(Input.GetAxis("Vertical2") >= 0.5f)
+        {
+            float Vertical = Input.GetAxis("Vertical2");
+            Debug.Log($"Vertical = {Vertical}");
+            Debug.Log("コントローラースティック反応しています");
+        }
+  
+        if(Input.GetAxis("Horizontal2") >= 0.5f)
+        {
+            float Horizontal = Input.GetAxis("Horizontal2");
+            Debug.Log($"Horizontal = {Horizontal}");
+            Debug.Log("コントローラースティック反応しています");
+        }
         // 接地しているとき
         if(charaCon.isGrounded)
         {
@@ -53,79 +73,4 @@ public class PlayController : MonoBehaviour
         // キャラクターを動かす
         charaCon.Move(moveVelocity * Time.deltaTime);
     }
-
-
-
-
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class PlayerController : MonoBehaviour
-// {
-//     private CharacterController characterController;  // CharacterController型の変数
-//     private Vector3 moveVelocity;  // キャラクターコントローラーを動かすためのVector3型の変数
-//     [SerializeField] private Animator animator;
-//     [SerializeField] public Transform verRot;  //縦の視点移動の変数(カメラに合わせる)
-//     [SerializeField] public Transform horRot;  //横の視点移動の変数(プレイヤーに合わせる)
-//     [SerializeField] public float moveSpeed;  //移動速度
-//     [SerializeField] public float jumpPower;  //ジャンプ力
-
-//     void Start()
-//     {
-//         characterController = GetComponent<CharacterController>();
-//     }
-
-//     void Update()
-//     {
-//         // マウスによる視点操作
-//         float X_Rotation = Input.GetAxis("Mouse X");
-//         float Y_Rotation = Input.GetAxis("Mouse Y");
-//         horRot.transform.Rotate(new Vector3(0, X_Rotation * 2, 0));
-//         verRot.transform.Rotate(-Y_Rotation * 2, 0, 0);
-
-//         //Wキーがおされたら
-//         if(Input.GetKey(KeyCode.W))
-//         {
-//             characterController.Move(this.gameObject.transform.forward * moveSpeed * Time.deltaTime);
-//         }
-//         //Sキーがおされたら
-//         if (Input.GetKey (KeyCode.S))
-// 		{
-// 			characterController.Move (this.gameObject.transform.forward * -1f * moveSpeed * Time.deltaTime);
-// 		}
-// 		//Aキーがおされたら
-// 		if (Input.GetKey (KeyCode.A))
-// 		{
-// 			characterController.Move (this.gameObject.transform.right * -1 * moveSpeed * Time.deltaTime);
-// 		}
-// 		//Dキーがおされたら
-// 		if (Input.GetKey (KeyCode.D))
-// 		{
-// 			characterController.Move (this.gameObject.transform.right * moveSpeed * Time.deltaTime);
-// 		}
-
-//         // 接地しているとき
-//         if(characterController.isGrounded)
-//         {
-//             // ジャンプ
-//             if(Input.GetKeyDown(KeyCode.Space))
-//             {
-//                 moveVelocity.y = jumpPower;
-//             }
-//         }
-//         // 空中にいる時
-//         else
-//         {
-//             // 重力をかける
-//             moveVelocity.y += Physics.gravity.y * Time.deltaTime;
-//         }
-
-//         // キャラクターを動かす
-//         characterController.Move(moveVelocity * Time.deltaTime);
-
-//         // 移動スピードをアニメーターに反映する
-//         animator.SetFloat("MoveSpeed", new Vector3(moveVelocity.x, 0, moveVelocity.z).magnitude);
-//     }
-// }
 }
